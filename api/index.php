15786c590e0eaf58f8e0c10c1eef9f962201a18d
Reza Arinda <rezaarinda26@gmail.com>
2026-04-08 15:10:11 +0700

    Redirect bootstrap caches to tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -7,6 +7,7 @@
 
 // Initialize /tmp/storage structure required by Vercel's read-only filesystem
 $tmpStorage = '/tmp/storage';
+$tmpCache = '/tmp/bootstrap/cache';
 
 if (!is_dir($tmpStorage)) {
     mkdir($tmpStorage, 0755, true);
@@ -15,6 +16,17 @@ if (!is_dir($tmpStorage)) {
     mkdir($tmpStorage . '/framework/sessions', 0755, true);
     mkdir($tmpStorage . '/logs', 0755, true);
 }
+
+if (!is_dir($tmpCache)) {
+    mkdir($tmpCache, 0755, true);
+}
+
+// Redirect all Laravel Cache Manifests to the writable /tmp folder
+$_ENV['APP_SERVICES_CACHE'] = $tmpCache . '/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $tmpCache . '/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $tmpCache . '/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $tmpCache . '/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = $tmpCache . '/events.php';
 
 // Pass control to Laravel
 require __DIR__ . '/../public/index.php';
